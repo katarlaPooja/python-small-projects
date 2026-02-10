2bffe07075ad4b1e087b74602a980221b20dfd39
Pooja Katarla <katarlapooja673@gmail.com>
2026-02-10 16:09:39 +0530

    Added student data structure with sample data for testing
    Initialized student management system with a sample student.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 print("Welcome to student management system ")
+students={}
+students["pooja"]={
+    "marks":90,
+    "status":"pass"
+}
+def show_menu():
+    print("\n1. Add student")
+    print("2. View students")
+    print("3. Exit")
+print(students)
+show_menu()
 print("project setup completed")
