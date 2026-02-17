36a52ca029af7e837a3f9af141830a8123e92455
Pooja Katarla <katarlapooja673@gmail.com>
2026-02-17 23:06:46 +0530

    Improve input validation and duplicate check
    Enhanced input validation for student name and marks. Improved student viewing logic. Enhanced duplicate check

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ print("Welcome to student management system ")
 students={}
 # Function to add student
 def add_student():
-    name = input("Enter the student name: ")
-    marks = int(input("Enter the marks: "))
+    name = input("Enter the student name: ").strip()
+    if not name:
+        print("Student name cannot be empty!")
+        return
+    if name in students:
+        print("Student already exists!")
+        return
 
-    if marks >= 50:
-        status = "PASS"
-    else:
-        status = "FAIL"
+    marks_input=input("enter marks(0_100):").strip()
+    if not marks_input.isdigit():
+        print("Invalid input! please enter numbers only.")
+        return
+    marks=int(marks_input)
+    if marks<0 or marks>100:
+      print("marks must be between 0to 100.")
+      return
+    status="Pass" if marks>=50 else "Fail"
 
     students[name] = {
         "marks": marks,
@@ -21,11 +31,12 @@ def add_student():
 def view_students():
     if not students:
         print("No student records found.")
-    else:
-        for name, details in students.items():
-            print(f"\nName: {name}")
-            print(f"Marks: {details['marks']}")
-            print(f"Status: {details['status']}")
+        return
+    print("\n---student records-----")
+    for name, details in students.items():
+        print(f"\nName: {name}")
+        print(f"Marks: {details['marks']}")
+        print(f"Status: {details['status']}")
 
 # Menu function
 def show_menu():
@@ -34,7 +45,7 @@ def show_menu():
         print("2. View students")
         print("3. Exit")
 
-        choice = input("Enter your choice: ")
+        choice = input("Enter your choice: ").strip()
 
         if choice == "1":
             add_student()
